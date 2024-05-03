b072332d4cab4676588924007b6200f7aa36fbe7
Owais Iqbal <owaisiqbal2013@gmail.com>
2024-05-03 23:18:04 +0530

    first commit

---
+++ src/test/java/com/example/promonade/PromonadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promonade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromonadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
