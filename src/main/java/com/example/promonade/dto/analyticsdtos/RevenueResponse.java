33e40a1856951cb041417ac9ae9c145711b125a2
Anagha-Kalyani <119789101+Anagha-Kalyani@users.noreply.github.com>
2024-05-28 01:30:20 +0530

    analytics

---
+++ src/main/java/com/example/promonade/dto/analyticsdtos/RevenueResponse.java
@@ -0,0 +1,10 @@
+package com.example.promonade.dto.response.analysticsdtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RevenueResponse {
+    private double totalRevenue;
+}
