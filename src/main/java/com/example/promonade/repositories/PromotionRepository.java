8c63d01f663e326f1f75eb41b666a940c9db0eaa
Anagha-Kalyani <119789101+Anagha-Kalyani@users.noreply.github.com>
2024-05-28 01:31:51 +0530

    Update PromotionRepository.java

--- src/main/java/com/example/promonade/repositories/PromotionRepository.java
+++ src/main/java/com/example/promonade/repositories/PromotionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
@@ -18,4 +19,8 @@ public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
 
     @Query("select p from Promotion p where p.approved = true")
     List<Promotion> findApprovedPromotions();
+
+    @Query("SELECT p FROM Promotion p WHERE p.validFrom <= :endDate AND p.validTill >= :startDate AND p.approved = true")
+    List<Promotion> findActivePromotionsInPeriod(Date startDate, Date endDate);
+
 }
